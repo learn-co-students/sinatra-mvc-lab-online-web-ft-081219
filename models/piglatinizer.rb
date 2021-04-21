1fa649e1bb0c3aada6379a5e90eef98e68a66b29
denalibalser <denali.balser167@gmail.com>
2021-04-21 14:33:24 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,36 @@
 
 class PigLatinizer 
-    def is_vowel?(word)
-        vowels = 'aeiouAEIOU'
-        vowels.include?(word[0])
-    end 
-
-    def piglatinize(word)
-        if self.is_vowel?(word)
-            word.concat('way')
-        else 
-            word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
-        end
-    end 
-    
-    def to_pig_latin(phrase)
-        phrase.split.collect{|word| piglatinize(word)}.join(' ')
-    end 
+    def piglatinize(thing)
+        pig(thing)
+      end
+      
+      def to_pig_latin(string)
+        pig(string)
+      end
+      
+      def pig(thing)
+        alpha = ('a'..'z').to_a + ('A'..'Z').to_a
+        vowels = %w[a e i o u] + %w[A E I O U]
+        consonants = alpha - vowels
+      
+        showme = []
+          input = thing.split(' ') 
+            input.each do |word|
+                if word.length < 2
+                   showme << word + "way" 
+                elsif vowels.include?(word[0])
+                   showme <<  word + "way"
+                elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+                   showme <<  word[3..-1] + word[0..2] + 'ay'
+                elsif consonants.include?(word[0]) && consonants.include?(word[1])
+                   showme <<  word[2..-1] + word[0..1] + 'ay'
+                elsif consonants.include?(word[0])
+                   showme <<  word[1..-1] + word[0] + 'ay'
+                else 
+                   showme <<  word + '?'
+                end
+            end 
+        showme.join(', ').gsub(/,/, '')
+      end
+       
 end 
